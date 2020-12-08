071ba7f5ace481988eb975d0f1c5aaa08e9e3882
Alex Marz <alexhmarz@gmail.com>
2020-12-08 15:01:57 -0500

    commit tree 817057fa8319b06b2dac415de274edc9c8e3dfb8

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @student = name
+        @grade = grade
+    if !@roster.include?(grade)
+        @roster[grade] = []
+        end
+        @roster[grade] << @student
+    end
+
+    def grade(grade_number)
+        @roster[grade_number]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            @roster[grade] = student.sort
+        end
+    end
+    
+
+
+
+
+end
